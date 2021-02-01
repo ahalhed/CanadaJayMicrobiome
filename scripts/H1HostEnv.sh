22eed1b02d5ca6c25cbc8a3fce1e9dde6a3d08f1
Alicia Halhed <55498260+ahalhed@users.noreply.github.com>
2021-02-01 16:14:39 -0500

    H1 Tidying

--- scripts/H1HostEnv.sh
+++ scripts/H1HostEnv.sh
@@ -2,12 +2,12 @@
 #SBATCH --account=def-cottenie
 #SBATCH --time=0-00:05:00
 #SBATCH --mem-per-cpu 1G
-#SBATCH --job-name=H1Spatial
-#SBATCH --output=./outputs/%x-%j.out
+#SBATCH --job-name=H1HostEnv
+#SBATCH --output=outputs/%x-%j.out
 
 
 # cd /home/ahalhed/projects/def-cottenie/Microbiome/GreyJayMicrobiome/
 module load nixpkgs/16.09 gcc/7.3.0 r/4.0.2
 
 # run R script
-Rscript /home/ahalhed/projects/def-cottenie/Microbiome/GreyJayMicrobiome/CanadaJayMicrobiome/scripts/H1Spatial.R
+Rscript /home/ahalhed/projects/def-cottenie/Microbiome/GreyJayMicrobiome/CanadaJayMicrobiome/scripts/H1HostEnv.R

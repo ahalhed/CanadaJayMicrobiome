55f538d9d1feeea9492a42f06c92a2b27858eea9
Alicia Halhed <55498260+ahalhed@users.noreply.github.com>
2021-02-11 12:01:08 -0500

    Update H2
    Need to divide stats by year/season

--- scripts/H2HostFactors.R
+++ scripts/H2HostFactors.R
@@ -17,16 +17,6 @@ library(tidyverse)
 theme_set(theme_bw())
 
 print("Initiate functions for analysis")
-# maximum distance
-max_dist <- function(dm) {
-  # dm is a distance matrix containing the distances between
-  # sampling locations
-  df1 <- as.data.frame(as.matrix(dm))
-  # find max in each column
-  summ <-  df1 %>% summarise_if(is.numeric, ~ max(., na.rm=TRUE))
-  m <- max(summ)
-  return(m)
-}
 # community object
 comm_obj <- function(XY, c) {
   # subset the OTUs (c is OTU table being subset)
@@ -56,10 +46,9 @@ met_filter <- function(meta, season, year) {
 print("Read in the Data")
 print("Building phyloseq object")
 gj_ps <- qza_to_phyloseq(features = "filtered-table-no-blanks.qza",
-                         taxonomy = "taxonomy/SILVA-taxonomy.qza",
                          metadata = "input/jay-met.tsv") %>%
   # transposing the OTU table into the format expected by vegan (OTUs as columns)
-  phyloseq(otu_table(t(otu_table(.)), taxa_are_rows = F), sample_data(.), tax_table(.))
+  phyloseq(otu_table(t(otu_table(.)), taxa_are_rows = F), sample_data(.))
 
 # based on the meta function from the microbiome package
 print("Extract the metadata")
@@ -98,17 +87,16 @@ print("Accessing the XY metadata by season")
 # loop to create individual season/year data frames
 for (YeaR in unique(gj_meta$CollectionYear)) {
   for (SeaSon in unique(gj_meta$CollectionSeason)) {
-  met <- rownames_to_column(gj_meta, var = "SampleID") %>% 
-    rename("Latitude"="LatitudeSamplingDD", "Longitude"="LongitudeSamplingDD")
-  df1 <- subset(met, CollectionSeason == SeaSon) %>%
-    subset(., CollectionYear == YeaR) %>%
-    select("SampleID", "Longitude", "Latitude") #dplyr select
-  df2 <- column_to_rownames(remove_rownames(df1), var = "SampleID")
-  assign(paste0("xy",SeaSon, YeaR),df2)
-  rm(met, df1, df2)
+    met <- rownames_to_column(gj_meta, var = "SampleID") %>% 
+      rename("Latitude"="LatitudeSamplingDD", "Longitude"="LongitudeSamplingDD")
+    df1 <- subset(met, CollectionSeason == SeaSon) %>%
+      subset(., CollectionYear == YeaR) %>%
+      select("SampleID", "Longitude", "Latitude") #dplyr select
+    df2 <- column_to_rownames(remove_rownames(df1), var = "SampleID")
+    assign(paste0("xy",SeaSon, YeaR),df2)
+    rm(met, df1, df2)
   }
 }
-
 # make a list of the xy data frames generated from the loop
 XY_list <- list(xyFall2017 = xyFall2017, xyFall2018 = xyFall2018, 
                 xyFall2020 = xyFall2020, xySpring2020 = xySpring2020)
@@ -123,45 +111,21 @@ print("Computing Haversine Distances")
 # using Haversine distance to get distance between sampling locations in meters
 # reasonable alternative to doing euclidean distances
 dist_list <- lapply(XY_list, function(x) distm(x, x, fun = distHaversine))
-print("Maximum Distance (m) by Collection Season")
-lapply(dist_list, max_dist)
 
-
-## community objects
-# subset the samples from the core microbiome
+## community object
 print("Build the community object (OTU table) for season")
-commFull <- lapply(XY_list, comm_obj, c=OTUclr)
+commFull <- lapply(sea_list, comm_obj, c=OTUclr)
 
 # unweighted PCNM
 print("Unweighted PCNM - for use with all OTU tables")
 pcnm_list <- lapply(dist_list, pcnm)
 lapply(pcnm_list, function(x) x$vectors)
-
-# put ordisurfs here
-# ordisurf(XY_list[["xyFall2017"]], scores(pcnm_list[["xyFall2017"]], choi=5), bubble = 4, col = "black", main = "PCNM 5")
-# ordisurf(XY_list[["xySpring2020"]], scores(pcnm_list[["xySpring2020"]], choi=8), bubble = 4, col = "black", main = "PCNM 8")
 # cleanup
-rm(OTUclr, comm_obj, max_dist, met_filter, XY_list)
+rm(OTUclr, comm_obj, met_filter, XY_list)
 
 # analysis really starts here
 print("Acessing PCNM scores")
 scores_list <- lapply(pcnm_list, scores)
-# analysis for all OTUs
-print("Analysis for All OTUs")
-print("Variance partitioning")
-vp_mod1_list <- mapply(varpart, commFull, scores_list, data=sea_list, 
-                       MoreArgs = list(~.),
-                       SIMPLIFY = FALSE)
-vp_mod1_list
-# plot the partitioning
-pdf(file = "CanadaJayMicrobiome/plots/AdditionalFigures/H1VPmod1.pdf")
-# make plot
-# plotted in numerical order by month
-lapply(vp_mod1_list, plot)
-dev.off()
-# clean up
-rm(vp_mod1_list, dist_list)
-
 # test with RDA
 print("Testing with RDA (full model)")
 # create a tiny anonymous function to include formula syntax in call
@@ -169,45 +133,6 @@ abFrac <- mapply(function(x,data) rda(x~., data),
                  commFull, sea_list, SIMPLIFY=FALSE)
 
 abFrac # Full model
-
-lapply(abFrac, anova, step=200, perm.max=1000)
-# RsquareAdj gives the same result as component [a] of varpart
-lapply(abFrac, RsquareAdj)
-
-# Test fraction [a] using partial RDA:
-print("Testing with partial RDA (fraction [a])")
-# create a tiny anonymous function to include formula syntax in call
-aFrac <- mapply(function(x,y,data) rda(x~.+Condition(scores(y)), data), 
-                commFull, pcnm_list, sea_list, SIMPLIFY=FALSE)
-aFrac
-# anova
-lapply(aFrac, anova, step=200, perm.max=1000)
-# RsquareAdj gives the same result as component [a] of varpart
-lapply(aFrac, RsquareAdj)
-
-# forward selection for parsimonious model
-print("Forward selection for parsimonious model")
-
-print("Prediction 1A")
-# spatial variables
-print("Spatial variables")
-pcnm_df <- lapply(pcnm_list, function(x) as.data.frame(scores(x)))
-bcFrac <- mapply(function(x,data) rda(x~., data), 
-                 commFull, pcnm_df, SIMPLIFY=FALSE) # Full model
-bcFrac0 <- mapply(function(x,data) rda(x~1, data), 
-                  commFull, pcnm_df, SIMPLIFY=FALSE) # Reduced model
-step.space <- mapply(function(x,y) ordiR2step(x, scope = formula(y)), 
-                     bcFrac0, bcFrac, SIMPLIFY=FALSE)
-step.space
-
-# summary of selection process
-print("Summary of spatial selection process")
-lapply(step.space, function(x) x$anova)
-print("ANOVA on full spatial selection")
-lapply(step.space, anova)
-
-# save plot
-
 
 print("Prediction 2A - Host associated factors")
 # create a tiny anonymous function to include formula syntax in call
@@ -231,16 +156,15 @@ dev.off()
 
 #cleanup
 # remove objects to be replaced/no longer needed
-rm(vdist,pbcd, commFull, sea_list, abFrac, aFrac, abFrac0,
-   step.env, pcnm_df, bcFrac, bcFrac0, step.space)
+rm(commFull, sea_list, abFrac, abFrac0, step.env)
 
 
 # distance based RDA using aitchison distance matrix
 
 # read in aitchison distance matrix
-dmAitchison <- read_qza("aitchison-distance.qza")
+dmAitchison <- read_qza("P2A-aitchison-distance.qza")
 # might switch to the phyloseq implementation
-gj_cap <- capscale(dmAitchison$data ~ ProportionSpruceOnTerritory + MeanTempC + CollectionSeason + CollectionYear,
+gj_cap <- capscale(dmAitchison$data ~ Sex + BirthYear + BreedingStatus + JuvenileStatus,
                    data = gj_meta, comm = otu_table(gj_ps), na.action = na.exclude)
 # look at summaries
 summary(gj_cap)
@@ -250,7 +174,7 @@ plot(gj_cap, main = "Aitchison Distance-based RDA")
 dev.off()
 
 # PERMANOVA
-adonis2(dmAitchison$data ~ TerritoryQuality + MeanTempC + CollectionSeason + CollectionYear + JayID,
+adonis2(dmAitchison$data ~ Sex + BirthYear + BreedingStatus + JuvenileStatus,
         data = gj_meta, na.action = na.exclude)
 # clean up
 rm(gj_meta, gj_ps, dmAitchison, gj_cap)

5b4184663aa3a5d4b0450f056d5ab2018d84b06a
Alicia Halhed <55498260+ahalhed@users.noreply.github.com>
2021-02-17 14:28:35 -0500

    H1 Territory

--- scripts/H1Territory.sh
+++ scripts/H1Territory.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cottenie
-#SBATCH --time=0-00:05:00
-#SBATCH --mem-per-cpu 1G
+#SBATCH --time=0-00:01:00
+#SBATCH --mem-per-cpu 500M
 #SBATCH --job-name=H1Territory
 #SBATCH --output=outputs/%x-%j.out
 

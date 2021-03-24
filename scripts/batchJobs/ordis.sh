b07cf02228839e8bed0dfdf2ee41f9fda0c99e88
Alicia Halhed <ahalhed@uoguelph.ca>
2021-03-24 13:47:57 -0400

    Ordis

--- scripts/batchJobs/ordis.sh
+++ scripts/batchJobs/ordis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cottenie
 #SBATCH --ntasks-per-node=16
-#SBATCH --time=0-01:00:00
+#SBATCH --time=0-03:00:00
 #SBATCH --mem-per-cpu 16G
 #SBATCH --job-name=ordis
 #SBATCH --output=CanadaJayMicrobiome/outputs/%x-%j.out

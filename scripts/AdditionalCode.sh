c75a5d89814f126f259ec8e66947e315589490b8
Alicia Halhed <55498260+ahalhed@users.noreply.github.com>
2021-03-24 14:37:23 -0400

    Additional

--- scripts/AdditionalCode.sh
+++ scripts/AdditionalCode.sh
@@ -11,4 +11,4 @@ module load nixpkgs/16.09 gcc/7.3.0 r/4.0.2
 
 # run R script
 Rscript /home/ahalhed/projects/def-cottenie/Microbiome/GreyJayMicrobiome/CanadaJayMicrobiome/scripts/AdditionalCode.R
-Rscript /home/ahalhed/projects/def-cottenie/Microbiome/GreyJayMicrobiome/CanadaJayMicrobiome/scripts/AdditionalFigures.R
+# additional figures run locally (map causing issues on graham)

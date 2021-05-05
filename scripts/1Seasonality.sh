5f3d4db86d7bb60d0b83d74cf0a01452d78226ef
Alicia Halhed <ahalhed@uoguelph.ca>
2021-05-05 08:38:58 -0400

    Updated outputs

--- scripts/1Seasonality.sh
+++ scripts/1Seasonality.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cottenie
 #SBATCH --time=0-00:05:00
-#SBATCH --mem-per-cpu 1G
+#SBATCH --mem-per-cpu 3G
 #SBATCH --job-name=1Seasonality
 #SBATCH --output=outputs/%x-%j.out
 
